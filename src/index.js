c7c59b46bab5e19aa0716fe5773291ff1b34a350
Alex Salazar <alxanderapollo@gmail.com>
2021-07-03 17:51:01 -0400

    First Commit, Simported reducer and providers as well as store change

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
+//import store, Provider, reducers, the app will be wrapped around the provider
+
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import App from './components/App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  //pass in our reducers
+  <Provider store = {createStore(reducers)}>
+    <App/>
+  </Provider>,
+  document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
